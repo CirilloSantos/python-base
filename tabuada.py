3495ce3e745bf7525fefc37e66efed31f6420c6b
CirilloSantos <cirillo.santos@gmail.com>
2024-02-18 12:24:39 -0300

    Tabuada do 1 ao 10

--- tabuada.py
+++ tabuada.py
@@ -4,4 +4,9 @@ __author__ = "CirilloSantos"
 
 #base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numeros = list(range(1, 11))
-print(numeros)
+
+for numero in numeros:
+    print("Tabuada do:", numero)    
+    for outro_numero in numeros:
+        print(numero * outro_numero)
+    print("-*-*-*-*-*-*-*-*-*-*")    
