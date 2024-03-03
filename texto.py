063032c21c263528afec599db8e4c0b785d4ffaa
CirilloSantos <cirillo.santos@gmail.com>
2024-03-02 22:54:02 -0300

    interpolação

--- texto.py
+++ texto.py
@@ -1,5 +1,28 @@
-nome = "Cirilo"
-saldo = 30.0
+email_tmpl = """
+Olá, %(nome)s
 
-print("O saldo do " + nome + " é o total de " + str(saldo))
+Tem interesse em comprar %(produto)s?
 
+Este produto é ótimo para resolver %(texto)s
+
+Clique agora em %(link)s
+
+Apenas %(quantidade)d disponivéis!
+
+Preço promocional %(preco).2f
+"""
+
+clientes = ["Maria", "Cirilo", "Bruno"]
+
+for cliente in clientes:
+    print(
+        email_tmpl
+        % {
+            "nome": cliente,
+            "produto": "tinta",
+            "texto": "Pinta muito bem",
+            "link": "https://tintas.com",
+            "quantidade": 20,
+            "preco": 45.5,
+        }
+    )
