731e4727069bc273812e118af589e28d4a4f3099
CirilloSantos <cirillo.santos@gmail.com>
2024-03-12 19:52:15 -0300

    Alterações de programas e formatação da tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,12 +1,37 @@
+"""Imprime a tabuada do 1 ao 10
 
-__version__ = "0.1.0"
+---Tabuada do 1---
+    
+    1 x 1 = 1
+    1 x 2 = 2
+    1 x 3 = 3
+...
+###################
+---Tabuada do 2---
+   
+    2 x 1 = 2
+    2 x 2 = 4
+    2 x 3 = 6
+...
+###################
+"""
+__version__ = "0.1.1"
 __author__ = "CirilloSantos"
 
-#base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+####################    
+
+# base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Iterable (percorriveis)
 numeros = list(range(1, 11))
 
-for numero in numeros:
-    print("Tabuada do:", numero)    
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("-*-*-*-*-*-*-*-*-*-*")    
+# para cada numero em numeros
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:   
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))  
+    print("#" * 18)    
+
+           
